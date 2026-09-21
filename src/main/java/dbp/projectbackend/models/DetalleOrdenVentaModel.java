afb98f4b2814a37e55f7519e4f00253840209ff4
Javier Bravo <javier.bravo@utec.edu.pe>
2026-09-21 00:30:53 -0500

    Model DetalleOrdenVenta

---
+++ src/main/java/dbp/projectbackend/models/DetalleOrdenVentaModel.java
@@ -0,0 +1,46 @@
+package dbp.projectbackend.models;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Setter
+@Getter
+@Entity
+@Table(name = "detalle_orden_venta")
+
+public class DetalleOrdenVentaModel {
+
+    public DetalleOrdenVentaModel(ProductModel producto, Integer cantidad, BigDecimal precioUnitario) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "orden_venta_id", nullable = false)
+    private OrdenVentaModel ordenVenta;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "producto_id", nullable = false)
+    private ProductModel producto;
+
+    @Column(nullable = false)
+    private Integer cantidad;
+
+    @Column(name = "precio_unitario", nullable = false)
+    private BigDecimal precioUnitario;
+
+    public BigDecimal getSubtotal() {
+        return precioUnitario.multiply(BigDecimal.valueOf(cantidad));
+    }
+}
