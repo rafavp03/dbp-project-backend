e9dde77afb39f7b51a0804a361ea4714a02c278c
rafavp03 <vpantonio03@gmail.com>
2026-09-25 19:15:04 -0500

    missing repositories

--- src/main/java/dbp/projectbackend/repositories/OrdenVentaRepository.java
+++ src/main/java/dbp/projectbackend/repositories/OrdenVentaRepository.java
@@ -2,7 +2,9 @@ package dbp.projectbackend.repositories;
 
 import dbp.projectbackend.models.OrdenVentaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrdenVentaRepository extends JpaRepository<OrdenVentaModel, Long> {
 
 }
